c06aa6e68ae1abbc1e12a10cacdea9d0804796db
dlsso <supertonesfan@hotmail.com>
2014-08-04 16:03:01 -0600

    Finished part 1 and 2, added wish list button

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 $(document).on('ready', function() {
+  	var templateSource = $('#template').html()
+  	var template = Handlebars.compile(templateSource)
   
+	for (var i = 0; i < productsData.productsList.length; i++) {
+		var product = productsData.productsList[i]
+		$('#product-container').append(template(product))
+	};
+
 });
